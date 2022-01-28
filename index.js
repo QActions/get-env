6919c1db59e887ec11ee6812546e0d008d8e6ea1
arnedubois <arne.dubois@extrahorizon.com>
2022-01-28 02:56:36 +0100

    Fix if else statements

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 
-const productionRegex = new RegExp('refs\/heads(\/v\d.*)?\/(master|main)')
-const stagingRegex = new RegExp('refs\/heads(\/v\d.*)?\/(staging|stage)')
-const releaseRegex = new RegExp('refs\/heads(\/v\d.*)?\/(release)')
+const productionRegex = /refs\/heads(\/v\d.*)?\/(master|main)/
+const stagingRegex = /refs\/heads(\/v\d.*)?\/(staging|stage)/
+const releaseRegex = /refs\/heads(\/v\d.*)?\/(release)/
 
 try {
   const ref = core.getInput('ref');
   console.log(`get-env started with ${ref} as input`)
   let environmentName;
-  if (productionRegex.test(environmentName)) {
-    core.setOutput("environmentname", 'production');
+  if (productionRegex.test(ref)) {
+    environmentName = 'production';
     console.log('Returned production environment');
-  }
-  else if (stagingRegex.test(environmentName)) {
-    core.setOutput("environmentname", 'staging');
+  } else if (stagingRegex.test(ref)) {
+    environmentName = 'staging';
     console.log('Returned staging environment');
-  }
-  else if (releaseRegex.test(environmentName)) {
-    core.setOutput("environmentname", 'release');
+  } else if (releaseRegex.test(ref)) {
+    environmentName = 'release';
     console.log('Returned release environment');
   } else {
-    core.setOutput("environmentname", 'development');
+    environmentName = 'development';
     console.log('Returned development environment');
   }
-  const time = (new Date()).toTimeString();
-  core.setOutput("environmentname", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  core.setOutput('environmentname', environmentName);
 } catch (error) {
   core.setFailed(error.message);
 }
